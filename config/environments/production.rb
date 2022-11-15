570c666ed52a1f6a48624fe4625e1ea9e3d96dec
Saumya Bhatnagar <saumyabhatnagar@SaumyaMacBook-Air-2.local>
2022-11-16 00:25:15 +0530

    added user authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,6 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.active_record.dump_schema_after_migration = false
 end
